19cdae055cd00f04ac1b0c6ca18bb9851f2ae2f6
rjwu95 <yugun000@gmail.com>
2020-08-24 15:13:43 +0900

    [feat] add horizontal moving that is acceleration on character
     - Using speedX and speedY value

--- src/app/Game.js
+++ src/app/Game.js
@@ -1,6 +1,34 @@
+import Player from "./Player";
+import Control from "./Control";
+const PLAYER_WIDTH = 30
+const PLAYER_HEIGHT = 30
+const PLAYER_COLOR = "red"
+
 class Game {
-  init () {
+  init() {
     this.canvas = document.getElementById('canvas');
+    this.canvas.width = 480;
+    this.canvas.height = 270;
+    this.context = this.canvas.getContext("2d");
+    this.player = new Player(10, 120);
+    this.updateGameArea()
+    this.control = new Control(this, this.player)
+    this.control.init()
+  }
+  clear() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+  renderPlayer() {
+    const ctx = this.context;
+    ctx.save()
+    ctx.fillStyle = PLAYER_COLOR;
+    ctx.fillRect(this.player.x, this.player.y, PLAYER_WIDTH, PLAYER_HEIGHT);
+    ctx.restore()
+  }
+  updateGameArea() {
+    this.clear();
+    this.player.newPos();
+    this.renderPlayer();
   }
 }
 
--- src/app/Player.js
+++ src/app/Player.js
@@ -1,4 +1,14 @@
 class Player {
+  constructor(x, y) {
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+  }
+  newPos() {
+    this.x += this.speedX
+    this.y += this.speedY
+  }
 }
 
 export default Player;
