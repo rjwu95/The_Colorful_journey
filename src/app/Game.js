d4e28ffb49e7028cb8b5f9d97a6c883970998377
judy-do <judy.do@kakaocorp.com>
2020-08-25 23:05:14 +0900

    [refactor] Resize window

--- src/app/Game.js
+++ src/app/Game.js
@@ -1,11 +1,11 @@
 import GameMap from './GameMap';
-import {BLOCK_SIZE, ROWS, COLUMNS} from '../constant/map';
+import {MAP_WIDTH, MAP_HEIGHT} from '../constant/map';
 import {level} from '../constant/level';
 class Game {
   init () {
     this.canvas = document.getElementById('canvas');
-    this.canvas.width = BLOCK_SIZE * COLUMNS;
-    this.canvas.height = BLOCK_SIZE * ROWS;
+    this.canvas.width = MAP_WIDTH;
+    this.canvas.height = MAP_HEIGHT;
     this.ctx = this.canvas.getContext('2d');
 
     this.map = new GameMap(this.ctx);
--- src/constant/map.js
+++ src/constant/map.js
@@ -1,5 +1,5 @@
-export const BLOCK_SIZE = 16;
+export const MAP_WIDTH = window.innerWidth;
+export const MAP_HEIGHT = window.innerHeight;
 export const ROWS = 32;
-export const COLUMNS = 32;
-export const MAP_WIDTH = ROWS * BLOCK_SIZE;
-export const MAP_HEIGHT = ROWS * BLOCK_SIZE;
+export const COLUMNS = 32
+export const BLOCK_SIZE = MAP_WIDTH / COLUMNS;
