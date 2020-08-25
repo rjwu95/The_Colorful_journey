10faf5ef7cfdec94736cb19486f140b6b0aed388
judy-do <judy.do@kakaocorp.com>
2020-08-25 23:21:07 +0900

    [refactor] Resize block size with calculate window ratio

--- src/constant/map.js
+++ src/constant/map.js
@@ -1,5 +1,6 @@
 export const MAP_WIDTH = window.innerWidth;
 export const MAP_HEIGHT = window.innerHeight;
+const MAP_RATIO = MAP_WIDTH/MAP_HEIGHT;
 export const ROWS = 32;
-export const COLUMNS = 32
+export const COLUMNS = Math.floor(ROWS * MAP_RATIO)
 export const BLOCK_SIZE = MAP_WIDTH / COLUMNS;
