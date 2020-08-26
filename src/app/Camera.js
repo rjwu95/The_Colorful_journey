3b0035b93ad238a7d1c20a6e726e8d375dd958b9
judy-do <judy.do@kakaocorp.com>
2020-08-26 23:51:26 +0900

    [feat] Add min/max camera position

--- src/app/Camera.js
+++ src/app/Camera.js
@@ -1,14 +1,23 @@
-import {MAP_WIDTH} from '../constant/map'
+import {MAP_WIDTH, TILE_MAP_WIDTH} from '../constant/map'
 class Camera {
   constructor() {
     this.cx = 0;
   }
 
   update(playerPos) {
+    const minCameraPos = 0;
+    const maxCameraPos = TILE_MAP_WIDTH - MAP_WIDTH;
+
     this.cx = playerPos - MAP_WIDTH/3;
 
-    if (this.cx < 0) {
-      this.cx = 0;
+    // camera min position
+    if (this.cx < minCameraPos) {
+      this.cx = minCameraPos;
+    }
+
+    // camera max position
+    if (this.cx > maxCameraPos) {
+      this.cx = maxCameraPos;
     }
   }
 }
--- src/constant/map.js
+++ src/constant/map.js
@@ -1,5 +1,6 @@
 export const MAP_WIDTH = window.innerWidth;
 export const MAP_HEIGHT = window.innerHeight;
+export const TILE_MAP_WIDTH = MAP_WIDTH * 2;
 const MAP_RATIO = MAP_WIDTH/MAP_HEIGHT;
 export const ROWS = 32;
 export const COLUMNS = Math.floor(ROWS * MAP_RATIO)
