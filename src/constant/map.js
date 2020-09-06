c4ea8b4836f6549e7194692d7094157a65098da8
judy-do <judy.do@kakaocorp.com>
2020-09-06 22:53:05 +0900

    [feat] change map width fix to 58 and player width responsible

--- src/constant/map.js
+++ src/constant/map.js
@@ -1,14 +1,16 @@
 export const MAP_WIDTH = window.innerWidth;
 export const MAP_HEIGHT = window.innerHeight;
+
 export const TILE_MAP_WIDTH = MAP_WIDTH * 2;
 export const TILE_MAP_HEIGHT = MAP_HEIGHT * 2;
 
-const MAP_RATIO = MAP_WIDTH/MAP_HEIGHT;
 export const ROWS = 32;
-export const COLUMNS = Math.floor(ROWS * MAP_RATIO)
+export const COLUMNS = 58;
+
 export const BLOCK_SIZE = MAP_WIDTH / COLUMNS;
-export const ITEM_SIZE = BLOCK_SIZE * 0.9;
+export const ITEM_SIZE = BLOCK_SIZE;
 export const BOX_SIZE = BLOCK_SIZE * 6;
+
 export const GAME_STATE = {
   GAME_READY: 'GAME_READY',
   GAME_PLAYING: 'GAME_PLAYING',
