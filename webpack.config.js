e7cf98fdee33107222bcda7bd79098443bd13aae
judy-do <judy.do@kakaocorp.com>
2020-09-13 15:31:12 +0900

    setting webpack for minify and zip

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,56 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ZipPlugin = require("zip-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 const isProduction = process.env.npm_lifecycle_event === 'build'
 
 module.exports = {
-  entry: './src',
-  devtool: !isProduction && 'source-map',
+  entry: "./src",
+  devtool: !isProduction && "source-map",
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.css?$/,
         use: [
           MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader'
           }
         ]
-      }
-    ]
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
+      template: "src/index.html",
       minify: isProduction && {
-        collapseWhitespace: true
+        removeComments: true,
+        removeAttributeQuotes: true,
+        removeScriptTypeAttributes: true,
+        removeTagWhitespace: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        inlineSource: ".(js)$",
       },
-      inlineSource: isProduction && '\.(js|css)$'
     }),
-    new HtmlWebpackInlineSourcePlugin(),
-    new OptimizeCssAssetsPlugin({}),
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    })
+    new ZipPlugin({ filename: "bundle.zip" })
   ],
+    optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          mangle: {
+            properties: {
+              keep_quoted: true,
+            },
+          },
+        },
+      }),
+    ],
+  },
   devServer: {
     stats: 'minimal',
     overlay: true
   }
-}
+};
